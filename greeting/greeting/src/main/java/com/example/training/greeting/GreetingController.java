e345f0727825d552801e34d91ef8c165c5deba19
geetakshi <geetakshi.teli@oracle.com>
2023-04-06 10:53:49 +0530

    first commit message

---
+++ greeting/greeting/src/main/java/com/example/training/greeting/GreetingController.java
@@ -0,0 +1,15 @@
+package com.example.training.greeting;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	@RequestMapping(path="/greet", method = RequestMethod.GET)
+	public String greet() {
+		return "Greetings! Welcome to Spring Boot Microservice";
+	}
+
+}
